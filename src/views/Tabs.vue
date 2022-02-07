1ed51a3d9ca9663d39c3b146679c02a8266a9371
Elizeu Braga <elizeubragasantos@gmail.com>
2022-02-07 14:28:48 -0300

    remove unusage menus

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,12 +1,7 @@
 <template>
   <ion-page>
     <ion-tabs>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button @click="tabChanged()" tab="tab1" href="/tabs/tab1">
-          <ion-icon color="primary" :icon="trendingUp" />
-          <ion-label>Home</ion-label>
-        </ion-tab-button>
-          
+      <ion-tab-bar color="primary" slot="bottom">  
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon color="danger" :icon="arrowUp"/>
           <ion-label>Saídas</ion-label>
@@ -15,11 +10,6 @@
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon color="success" :icon="arrowDown" />
           <ion-label>Entradas</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button disabled tab="tab4" href="/tabs/tab4">
-          <ion-icon color="primary" :icon="menu" />
-          <ion-label>Menu</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
