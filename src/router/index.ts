64fab5a8226536fd57ab919362dc277c3ae3c229
Elizeu Braga <elizeubragasantos@gmail.com>
2022-02-07 14:46:29 -0300

    remove unusage menus

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import Tabs from '../views/Tabs.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/tab2'
   },
   {
     name: 'Login',
