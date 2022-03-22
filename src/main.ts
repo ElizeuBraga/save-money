19c0b8fd49962e0f96b2f21562cf9de5228a89ae
Elizeu Braga <elizeubragasantos@gmail.com>
2022-03-22 17:19:02 -0300

    remove unusage arquivs and firebase

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,9 @@
+import { createApp } from 'vue'
+import { IonicVue } from '@ionic/vue';
+
 import App from './App.vue'
 import router from './router';
-import { createApp } from 'vue'
 
-import { 
-  IonCol,
-  IonRow,
-  IonGrid,
-  IonCard,
-  IonItem,
-  IonPage, 
-  IonLabel, 
-  IonicVue, 
-  IonTitle,
-  IonInput,
-  IonButton,
-  IonHeader,
-  IonToolbar,
-  IonContent,
-  IonCardTitle,
-  IonCardHeader,
-  IonProgressBar,
-  IonCardContent,
-  toastController,
-  IonRouterOutlet
-} from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -48,99 +28,16 @@ import './theme/custom.css';
 
 import './registerServiceWorker';
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { Timestamp } from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: process.env.VUE_APP_FIRE_API_KEY,
-    authDomain: process.env.VUE_APP_FIRE_AUTH_DOMAIN,
-    databaseURL: process.env.VUE_APP_FIRE_DB_URL,
-    projectId: process.env.VUE_APP_FIRE_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_FIRE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FIRE_MESSAGING_SENDER_ID,
-    appId: process.env.VUE_APP_FIRE_APP_ID,
-    measurementId: process.env.VUE_APP_FIRE_MEASUREMENT_ID
-};
-
-/* eslint-disable */ 
-
-initializeApp(firebaseConfig);
-
-const app = createApp(App)
-app.use(IonicVue)
-app.use(router)
+const app = createApp(App).use(IonicVue).use(router)
 
 app.mixin({
-  components:{
-    IonCol,
-    IonRow,
-    IonGrid,
-    IonCard,
-    IonItem,
-    IonPage, 
-    IonLabel, 
-    IonTitle,
-    IonInput,
-    IonButton,
-    IonHeader,
-    IonToolbar,
-    IonContent,
-    IonCardTitle,
-    IonCardHeader,
-    IonProgressBar,
-    IonCardContent,
-    IonRouterOutlet
-  },
-  data:()=>{
-    return{
-      actualize: false,
-      milliseconds: Timestamp.now().toMillis()
-    }
-  },
-
   methods:{
-    async showToast(color: string, message: string) {
-      const toast = await toastController.create({
-        message: message,
-        duration: 2000,
-        position: "top",
-        color: color,
-      });
-      return toast.present();
-    },
-
     formatMoney(amount: any) {
       if(amount){
         return parseFloat(amount).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
       }else{
         return "R$ " + parseFloat("0.00").toFixed(2).replace(".", ",");
       }
-    },
-
-    formatDate(date: any, expiration = false){
-      const day = new Date(date).getDate();
-      if(!date || isNaN(day)){
-        return '-'
-      }
-      
-      let dateFormated = "dia " + day;
-      const today = new Date().getDate();
-      if(!expiration){
-        if(today == day){
-          dateFormated = "Hoje";
-        }
-        if((today-1) == day){
-          dateFormated = "Ontem";
-        }
-      }
-
-      return dateFormated;
-    },
-
-    async getUid(){
-      const auth = getAuth()
-      return auth.currentUser!.uid;
     }
   }
 })
--- src/router/index.ts
+++ src/router/index.ts
@@ -53,28 +53,5 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  if(to.name !== 'Login' && to.name !== 'Register' && to.name !== 'RecoverPassword'){
-    const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = user.uid;
-          
-          next()
-          // ...
-        } else {
-          // User is signed out
-          // ...
-          next('/login')
-          console.log('Não está logado')
-        }
-      })
-    }else{
-      next()
-    }
-  })
-
-  export default router
+export default router
   
